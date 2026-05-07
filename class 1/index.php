d62b6196ce7925944c7e5bb3a34e4012e22dcaf2
saad-mamun <mamunali2660@gmail.com>
2026-05-07 07:58:57 +0600

    first commit

---
+++ class 1/index.php
@@ -0,0 +1,17 @@
+<?php
+echo "Hello world!";
+echo "This is a test.";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Hello, World!</title>
+</head>
+<body style="background-color: black; color: white" >
+    <br>
+    <button>Order Pizza</button>
+</body>
+</html>
