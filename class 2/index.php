86d8eb12c6517ac0d262dfc28808403fb2ce83a7
saad-mamun <mamunali2660@gmail.com>
2026-05-07 17:39:52 +0600

    integer/float/boolean

--- class 2/index.php
+++ class 2/index.php
@@ -1,4 +1,42 @@
 <?php
+// Variable in PHP..
 $name = "Liton Ali";
-echo "Hello world! My name is $name.";
+$food = "Pizza";
+$email = "deco@gmail.com";
+
+// Integer
+$age = 22;
+
+// Float
+$gpa = 3.26;
+$price = 5.99;
+$tax = 0.07;
+
+// Boolean
+$employee = true;
+$online = false;
+$for_sale = true;
+
+
+echo "Hello world! My name is $name.<br>";
+echo "I love {$food}s.<br>";
+echo "You can contact me at $email.<br>";
+
+echo "I am $age years old.<br>";
+
+echo "My GPA is $gpa.<br>";
+echo "The price of the item is $$price.<br>";
+echo "The tax is $$tax%.<br>";
+
+echo "Am I an employee? " . ($employee ? "Yes" : "No") . "<br>";
+echo "Am I online? " . ($online ? "Yes" : "No") .   "<br>";
+echo "Is the item for sale? " . ($for_sale ? "Yes" : "No") . "<br>";
+                                
+
+
+
+
+
+
+
 ?>
