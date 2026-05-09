93e166a096583788bbda9aaca20142117a286ba9
saad-mamun <mamunali2660@gmail.com>
2026-05-09 17:11:36 +0600

    post

--- class 4/index.php
+++ class 4/index.php
@@ -8,7 +8,7 @@
 </head>
 
 <body>
-    <form action="index.php" method="post">
+    <!-- <form action="index.php" method="post">
         <label>Username:</label><br>
         <input type="text" name="username"><br>
 
@@ -16,14 +16,39 @@
         <input type="password" name="password"><br>
 
         <button type="submit">Login</button>
+    </form> -->
+
+
+
+
+<!-- form 2 -->
+    <form action="index.php" method="post">
+        <label>Quantity:</label><br>
+        <input type="text" name="quantity"><br>
+        <button type="submit" value="total">Submit</button>
     </form>
 </body>
 
 </html>
 
 <?php
-echo "{$_POST["username"]} <br>";
-echo "{$_POST["password"]} <br>";
+// echo "{$_GET["username"]} <br>";
+// echo "{$_GET["password"]} <br>";
+
+// echo "{$_POST["username"]} <br>";
+// echo "{$_POST["password"]} <br>";
+
+
+
+// form 2
+$item = "Pizza";
+$price = 4.99;
+$quantity = $_POST["quantity"];
+$total = null;
+
+$total = $price * $quantity;
+echo "You ordered $quantity $item(s) <br>";
+echo "Total: $$total";
 
 
 ?>  
