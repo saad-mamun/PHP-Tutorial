f9aa5b630eb49efdb70b6ce3ed7c491d221400fb
saad-mamun <mamunali2660@gmail.com>
2026-05-11 20:16:41 +0600

    D

--- class 3/index.php
+++ class 3/index.php
@@ -8,7 +8,7 @@ $b = 5;
 echo $a + $b . "<br>"; // Addition
 echo $a - $b . "<br>"; // Subtraction
 echo $a * $b . "<br>"; // Multiplication
-echo $a / $b . "<br>"; // Division
+echo $a / $b . "<br>"; 
 echo $a % $b . "<br>"; // Modulus
 echo $a ** $b . "<br>"; // Exponentiation
 
