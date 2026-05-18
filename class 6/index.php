98dde2bdaa68bbc04feb60fe78710770984e6d41
saad-mamun <mamunali2660@gmail.com>
2026-05-18 17:34:26 +0600

    if statement with bolean

--- class 6/index.php
+++ class 6/index.php
@@ -1,21 +1,34 @@
 <?php
+
+// example 1:  of if, elseif and else statement
+
 $age =  20;
 
+$adult = true;
+// if($age >= 100){
+//     echo "You are too old to enter this site";
+// }
+// elseif($age >= 18){
+//     echo "You may enter this site";
+// }
+// elseif($age <= 0){
+//     echo "You are not born yet";
+// }
 
-if($age >= 100){
-    echo "You are too old to enter this site";
-}
-elseif($age >= 18){
+// else{
+//     echo "You must be 18+ to enter this site";
+// }
+
+
+
+//example 2: of if, elseif and else statement
+
+if($adult){
     echo "You may enter this site";
 }
-elseif($age <= 0){
-    echo "You are not born yet";
-}
-
 else{
     echo "You must be 18+ to enter this site";
 }
-
 
 
 
