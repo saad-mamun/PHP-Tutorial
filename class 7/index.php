e97168bd90245cc3f62cd32a4f187de4b47b94ba
saad-mamun <mamunali2660@gmail.com>
2026-05-19 17:43:23 +0600

    ! oparetor

--- class 7/index.php
+++ class 7/index.php
@@ -1,14 +1,23 @@
 <?php
 
 $temp = 20;
+$cloudy = false;
 
-if ($temp < 0 || $temp > 30){
-    echo "Weather is bad";;
+
+
+if ($temp > 0 || $temp < 30){
+    echo "Weather is good. <br>";
 }
 else{
-    echo "Weather is good";
+    echo "Weather is bad. <br>";
 }
 
+if (!$cloudy ){
+    echo "Its cloudy. <br>";
+}
+else{
+    echo "Its not cloudy. <br>";
+}
 
 
 
