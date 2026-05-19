8a0d6967f44e0a52d872bafd53c2edf10416166e
saad-mamun <mamunali2660@gmail.com>
2026-05-19 17:53:44 +0600

    exercies

--- class 7/index.php
+++ class 7/index.php
@@ -3,6 +3,14 @@
 $temp = 20;
 $cloudy = false;
 
+$age = 20;
+$citizen = false;
+
+$child = false;
+$senior = false;
+$ticket = null;
+
+
 
 
 if ($temp > 0 || $temp < 30){
@@ -18,6 +26,29 @@ if (!$cloudy ){
 else{
     echo "Its not cloudy. <br>";
 }
+
+
+if($age >=18 && $citizen){
+    echo " You can vote. <br>";
+}
+else{
+    echo " You cannot vote. <br>";
+}
+
+
+if($child || $senior){
+    $ticket = 10;
+    echo "You get a discount $$ticket. <br>";
+}
+else{
+    $ticket = 15;
+    echo "You pay $$ticket. <br>";
+}
+
+
+
+
+
 
 
 
