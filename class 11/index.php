0e76c64ab3a8db4999f4548fac95a6a4dde7ba87
saad-mamun <mamunali2660@gmail.com>
2026-05-26 23:52:02 +0600

    push and pop

--- class 11/index.php
+++ class 11/index.php
@@ -3,10 +3,16 @@
 
 
 $foods = array("Pizza", "Burger", "Pasta", "Salad");
+
 // echo $foods[0] . "<br>";
 // echo $foods[1] . "<br>";
 // echo $foods[2] . "<br>";
 // echo $foods[3] . "<br>";
+
+
+// $foods[0] = "Sushi"; 
+// array_push($foods, "Pineapple", "Ice Cream");
+array_pop($foods);
 
 foreach( $foods as $food){
     echo $food . "<br>";
