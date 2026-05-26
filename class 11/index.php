9664c350abad8b7d4b21b7d7ee0aeca4f2193b90
saad-mamun <mamunali2660@gmail.com>
2026-05-26 23:45:05 +0600

    Array

---
+++ class 11/index.php
@@ -0,0 +1,13 @@
+<?php
+// array
+
+
+$foods = array("Pizza", "Burger", "Pasta", "Salad");
+echo $foods[0] . "<br>";
+echo $foods[1] . "<br>";
+echo $foods[2] . "<br>";
+echo $foods[3] . "<br>";
+
+
+
+?>
