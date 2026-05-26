aa0cb0b0cf2774558008f1800b516ee9f5e5173f
saad-mamun <mamunali2660@gmail.com>
2026-05-26 23:47:59 +0600

    foreach in array

--- class 11/index.php
+++ class 11/index.php
@@ -3,10 +3,14 @@
 
 
 $foods = array("Pizza", "Burger", "Pasta", "Salad");
-echo $foods[0] . "<br>";
-echo $foods[1] . "<br>";
-echo $foods[2] . "<br>";
-echo $foods[3] . "<br>";
+// echo $foods[0] . "<br>";
+// echo $foods[1] . "<br>";
+// echo $foods[2] . "<br>";
+// echo $foods[3] . "<br>";
+
+foreach( $foods as $food){
+    echo $food . "<br>";
+}
 
 
 
