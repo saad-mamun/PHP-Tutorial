56e9276105f9b1d80f6a55ffbe35a15b0e0ebb0d
saad-mamun <mamunali2660@gmail.com>
2026-05-27 23:43:03 +0600

    associative function

--- class 12/index.php
+++ class 12/index.php
@@ -8,8 +8,34 @@ $capitals = array(
     "India" => "New Delhi"
 );
 
+// echo $capitals["USA"]. "<br>";
+// $capitals["USA"] = "New York"; //update value
+// $capitals["CANADA"] = "Ottawa"; //add new key-value pair
+// array_pop($capitals);
 
-echo $capitals["USA"]. "<br>";
+// $keys = array_keys($capitals); //get all keys
+// $values = array_values($capitals); //get all values
+// $capitals = array_flip($capitals); //flip keys and values
+// $capitals = array_reverse($capitals); //reverse array
+echo count($capitals); //count array elements
 
+
+// foreach($keys as $key){
+//     echo $key . "<br>";
+// };
+
+// foreach($values as $value){
+//     echo $value . "<br>";
+// };
+
+foreach($capitals as $capital => $country){
+    echo " {$country} = {$capital}<br>";
+}
+
+
+
+foreach($capitals as $country => $value){
+    echo "The capital of {$country} is {$value}<br>";
+}
 
 ?>
