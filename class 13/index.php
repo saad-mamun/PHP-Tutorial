c3a5181712552d14ee47681bcc2eb18303e58d63
saad-mamun <mamunali2660@gmail.com>
2026-05-28 19:20:00 +0600

    form example

--- class 13/index.php
+++ class 13/index.php
@@ -1,3 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" name="username" placeholder="Username"><br/>
+        <label for="password">Password:</label>
+        <input type="password" name="password" placeholder="Password">
+        <input type="submit" value="Log In">    
+    </form>
+</body>
+</html>
+
+
+
+
 <?php
 // isset();
 // empty();
@@ -15,11 +36,28 @@ if(isset($username)){
 
 
 if(empty($username)){
-    echo "Username is empty";   
+    // echo "Username is empty";   
 }else{
-    echo "Username is NOT empty";
+    // echo "Username is NOT empty";
 };
 
+
+
+// for form
+if(isset($_POST['username']) && isset($_POST['password'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if(empty($username) || empty($password)){
+        echo "Username and password cannot be empty";
+    }else{
+        echo "Username: " . $username . "<br/>";
+        echo "Password: " . $password;
+    }
+
+}else{
+    echo "Please fill in the form"; 
+}
 
 
 
