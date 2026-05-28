e068a2b4537de836be27359eedc0201b28c59154
saad-mamun <mamunali2660@gmail.com>
2026-05-28 19:07:33 +0600

    isset and empty

---
+++ class 13/index.php
@@ -0,0 +1,26 @@
+<?php
+// isset();
+// empty();
+
+
+$username = null; // empty
+// echo isset($username); // true
+
+
+if(isset($username)){
+    // echo "Username is set";
+}else{
+    // echo "Username is NOT set";
+};
+
+
+if(empty($username)){
+    echo "Username is empty";   
+}else{
+    echo "Username is NOT empty";
+};
+
+
+
+
+?>
