21ab6f12f1385a9d5621c3a0082d0c043228d3e0
saad-mamun <mamunali2660@gmail.com>
2026-05-29 23:09:39 +0600

    parameter and argument

---
+++ class 16/index.php
@@ -0,0 +1,27 @@
+<?php
+// function happy_birthday(){
+//     echo "Happy Birthday! <br>";
+//     echo "Happy Birthday to you! <br>";
+//     echo "Happy Birthday to my dear friend! <br>";
+//     echo "Happy Birthday! <br>";
+// }
+// happy_birthday();
+
+
+
+// parameter 
+function happy_birthday($first_name, $age){
+    echo "Happy Birthday $first_name! <br>";
+    echo "Happy Birthday to $first_name! <br>";
+    echo "Happy Birthday to my dear friend $first_name! <br>";
+    echo "Happy Birthday $first_name! <br>";
+    echo "You are now $age! <br> <br>";
+}
+
+happy_birthday("Liton ", 22);
+happy_birthday("Al Mamun", 22);
+happy_birthday("Naima", 23);
+
+
+
+?>
