65099fd2d3997ece3510b89d50fdcab2fb257d85
saad-mamun <mamunali2660@gmail.com>
2026-05-29 16:09:21 +0600

    switch and if else

--- class 14/index.php
+++ class 14/index.php
@@ -20,16 +20,63 @@
 
 <?php
 
+// role 1:
 
+// if (isset($_POST['submit'])) {
+
+//     if (isset($_POST['card'])) {
+//         $card = $_POST['card'];
+//         echo " You have selected: " . $card;
+
+//     } else {
+//         echo "Please select a card type.";
+//     }
+// }
+
+
+// role 2:
+
+// if (isset($_POST['submit'])) {
+//     $card = null;
+
+//     if (isset($_POST["card"])) {
+//         $card = $_POST['card'];
+//     }
+
+//     if ($card == "Visa") {
+//         echo "You have selected Visa.";
+//     } elseif ($card == "MasterCard") {
+//         echo "You have selected MasterCard.";
+//     } elseif ($card == "American Express") {
+//         echo "You have selected American Express.";
+//     } else {
+//         echo "Please select a card type.";
+//     }
+// }
+
+// role 3:
 
 if (isset($_POST['submit'])) {
+    $card = $_POST['card'] ?? null;
 
-    if (isset($_POST['card'])) {
-        $card = $_POST['card'];
-        echo " You have selected: " . $card;
-
-    } else {
-        echo "Please select a card type.";
+    switch ($card) {
+        case "Visa":
+            echo "You have selected Visa.";
+            break;
+        case "MasterCard":
+            echo "You have selected MasterCard.";
+            break;
+        case "American Express":
+            echo "You have selected American Express.";
+            break;
+        default:
+            echo "Please select a card type.";
     }
 }
+
+
+
+
+
+
 ?>
