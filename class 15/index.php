c23a5e916cb07fa28e91bec356996cfa28fc67c9
saad-mamun <mamunali2660@gmail.com>
2026-05-29 16:19:42 +0600

    checkbox

---
+++ class 15/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Check box</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+        <input type="checkbox" name="pizza" value="Pizza">Pizza<br>
+        <input type="checkbox" name="hamburger" value="Hamburger">Hamburger<br>
+        <input type="checkbox" name="hotdog" value="Hotdog">Hotdog<br>
+        <input type="checkbox" name="taco" value="Taco">Taco<br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
+
+</html>
+
+<?php
+if (isset($_POST['submit'])) {
+
+    if (isset($_POST['pizza'])) {
+        echo "You like pizza!<br>";
+    }
+    if (isset($_POST['hamburger'])) {
+        echo "You like hamburger!<br>";
+    }
+    if (isset($_POST['hotdog'])) {
+        echo "You like hotdog!<br>";
+    }
+    if (isset($_POST['taco'])) {
+        echo "You like taco!<br>";
+    }
+}
+
+
+
+?>
