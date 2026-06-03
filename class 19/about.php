8f64b4fd494c38aea639539cba218ba002f70791
saad-mamun <mamunali2660@gmail.com>
2026-06-03 21:43:12 +0600

    include function

---
+++ class 19/about.php
@@ -0,0 +1,28 @@
+<?php
+
+include 'header.html';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <h1>Include Function</h1>
+        <p>This is the About page of my website.</p>
+    </div>
+</body>
+
+</html>
+<?php
+
+include 'footer.html';
+
+?>
