368982515d0f655cbe3277910dc6c137210bf02d
saad-mamun <mamunali2660@gmail.com>
2026-06-03 21:20:23 +0600

    sanitize

--- class 17/index.php
+++ class 17/index.php
@@ -3,7 +3,7 @@
 $username = "Mamun ali";
 
 // $username = strtolower($username);
-// $username = strtoupper($username);
+$username = strtoupper($username);
 // $username = trim($username);
 // $username = str_replace($username, " ", "Naima Ali");
 // $username = substr($username, 0, 7); //0 theke start kore 7 porjonto show korbe
@@ -16,7 +16,7 @@ $username = "Mamun ali";
 // $username = str_pad($username, 20, "*"); //string er por koto ta space dite hobe seta bole dite hoy
 // $username = str_shuffle($username); //string er letter gulo ke random vabe shuffle kore
 // $username = str_repeat($username, 3); //string ke koto bar repeat korte hobe seta bole dite hoy
-$username = str_split($username); //string ke array te convert kore
+// $username = str_split($username); //string ke array te convert kore
 
 
 echo $username;
---
+++ class 18/index.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sanitize / Validate</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <input type="text" name="username" placeholder="Enter username"><br>
+        <input type="email" name="email" placeholder="Enter email"><br>
+        <input type="number" name="age" placeholder="Enter age"><br>
+        <input type="submit" name="login" value="Login">
+    </form>
+</body>
+</html>
+
+<?php
+
+if(isset($_POST['login'])){
+
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $age = $_POST['age'];
+    // echo "Hello {$username} <br>";
+    // echo "Email: {$email}";
+
+    //validate
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_NUMBER_INT);
+
+    echo "Hello {$username} <br>";
+    echo "Email: {$email} <br>";
+    echo "Age: {$age} <br>";
+
+}
+
+?>
