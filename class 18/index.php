f4c521d503ded1a48a5107617b9a01f31ee90088
saad-mamun <mamunali2660@gmail.com>
2026-06-03 21:29:00 +0600

    validate

--- class 18/index.php
+++ class 18/index.php
@@ -26,14 +26,25 @@ if(isset($_POST['login'])){
     // echo "Hello {$username} <br>";
     // echo "Email: {$email}";
 
-    //validate
-    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
-    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-    $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_NUMBER_INT);
+    //sanitizeing data
+    // $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    // $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    // $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_NUMBER_INT);
+    // echo "Hello {$username} <br>";
+    // echo "Email: {$email} <br>";
+    // echo "Age: {$age} <br>";
 
-    echo "Hello {$username} <br>";
-    echo "Email: {$email} <br>";
-    echo "Age: {$age} <br>";
+    //validate
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    $age = filter_input(INPUT_POST, 'age', FILTER_VALIDATE_INT);
+
+  if(empty($age)){
+    echo "THAT IS NOT A NUMBER";
+  }else{
+    echo "You are {$age} years old";
+  }
+
+
 
 }
 
