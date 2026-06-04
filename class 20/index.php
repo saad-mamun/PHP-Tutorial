0d58e6b3510741f2882e3604392f86d4a5766dbc
saad-mamun <mamunali2660@gmail.com>
2026-06-04 15:34:03 +0600

    example

--- class 20/index.php
+++ class 20/index.php
@@ -3,8 +3,22 @@
 setcookie("fav_food", "Pizza", time() + (86400 * 2), "/");
 setcookie("fav_drink", "Coca Cola", time() + (86400 * 2), "/");
 
+/*
 foreach ($_COOKIE as $key => $value){
     echo " {$key} = {$value}  <br> ";
 }
+    */
 
-?>
+if (isset($_COOKIE["fav_food"])) {
+    echo "Your favorite food is: " . $_COOKIE["fav_food"] . "<br>";
+    echo "So, Buy some {$_COOKIE["fav_food"]}! <br>  <br> ";
+} else {
+    echo "Favorite food cookie is not set.<br>  <br> ";
+}
+
+if (isset($_COOKIE["fav_drink"])) {
+    echo "Your favorite drink is: " . $_COOKIE["fav_drink"] . "<br>";
+    echo "So, Buy some {$_COOKIE["fav_drink"]}! <br>  <br> ";
+} else {
+    echo "Favorite drink cookie is not set.<br>  <br> ";
+}
