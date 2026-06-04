b2fd8c246d812878cd532baeffb25b7541d27496
saad-mamun <mamunali2660@gmail.com>
2026-06-04 15:29:25 +0600

    using foreach cookie

--- class 20/index.php
+++ class 20/index.php
@@ -3,4 +3,8 @@
 setcookie("fav_food", "Pizza", time() + (86400 * 2), "/");
 setcookie("fav_drink", "Coca Cola", time() + (86400 * 2), "/");
 
+foreach ($_COOKIE as $key => $value){
+    echo " {$key} = {$value}  <br> ";
+}
+
 ?>
