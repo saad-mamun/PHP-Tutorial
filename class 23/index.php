6746a1cbfacb0c60d887c249702955ffa8179331
saad-mamun <mamunali2660@gmail.com>
2026-06-07 17:54:39 +0600

    hash

---
+++ class 23/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$password = "litonALI123";
+
+$hash = password_hash($password, PASSWORD_DEFAULT);
+// echo $hash;
+
+
+// password and hash jodi match kore tahole password_verify() function true return korbe, na hole false return korbe.
+if (password_verify($password, $hash)){
+    echo "Password is valid!";
+} else {
+    echo "Invalid password.";
+}
+
+
+?>
