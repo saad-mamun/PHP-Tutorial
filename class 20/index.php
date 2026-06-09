93ad721b61435378f17f87ece6aa459f41b31668
saad-mamun <mamunali2660@gmail.com>
2026-06-09 22:58:53 +0600

    A

--- class 20/index.php
+++ class 20/index.php
@@ -1,7 +1,7 @@
 <?php
 
 setcookie("fav_food", "Burger", time() + (86400 * 2), "/");
-setcookie("fav_drink", "Coca Cola", time() + (86400 * 2), "/");
+setcookie("fav_drink", "Mojo", time() + (86400 * 2), "/");
 
 /*
 foreach ($_COOKIE as $key => $value){
