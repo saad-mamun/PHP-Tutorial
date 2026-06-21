3979a41c027e9d7698798ef10b5f4b9d57cd554a
saad-mamun <mamunali2660@gmail.com>
2026-06-21 21:50:55 +0600

    A

--- class 20/index.php
+++ class 20/index.php
@@ -10,7 +10,7 @@ foreach ($_COOKIE as $key => $value){
     */
 
 if (isset($_COOKIE["fav_food"])) {
-    echo "Your favorite food is: " . $_COOKIE["fav_food"] . "<br>";
+    echo "Your favorite food is : " . $_COOKIE["fav_food"] . "<br>";
     echo "So, Buy some {$_COOKIE["fav_food"]}! <br>  <br> ";
 } else {
     echo "Favorite food cookie is not set.<br>  <br> ";
