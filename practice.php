ded12341d96188a241ebd166626802343732d059
saad-mamun <mamunali2660@gmail.com>
2026-06-22 17:20:50 +0600

    if else practice

---
+++ practice.php
@@ -0,0 +1,16 @@
+<?php
+
+$age = 30;
+
+
+if($age > 20){
+    echo "You can ride the bike";
+}
+elseif($age == 20){
+    echo "carefully ride the bike";
+}
+else{
+    echo "You cannot ride the bike";
+}
+
+?>
