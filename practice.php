90cac32931267ef7aa448c3cfa0d1e0458cbe690
saad-mamun <mamunali2660@gmail.com>
2026-06-25 08:37:31 +0600

    array

--- practice.php
+++ practice.php
@@ -1,29 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="practice.php" method="post" >
-        <input type="submit" value="Stop" name="stop">
-    </form>
-</body>
-</html>
-
-
-
 <?php
 
-$counter = 0;
+$countries = array("USA", "Canada", "Mexico", "Brazil", "Argentina");
 
-while($counter < 10){
-    $counter++;
-    echo $counter . "<br>";
+foreach( $countries as $country){
+     echo $country . "<br>";
 }
 
+echo $countries[0] . "<br>";
 
-
+echo $countries[3] . "<br>";
+echo count($countries) . "<br>";
 
 ?>
