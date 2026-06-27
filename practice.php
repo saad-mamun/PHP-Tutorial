b0e6c54c612c7d5f421c788b3a14ea07dd111f15
saad-mamun <mamunali2660@gmail.com>
2026-06-27 23:47:08 +0600

    satisfying practice

--- practice.php
+++ practice.php
@@ -10,12 +10,13 @@
 <body>
 
      <form action="practice.php" method="post">
-          <input type="radio" name="MobileBank" value="Bkash"> Bkash <br />
-          <input type="radio" name="MobileBank" value="Nagad"> Nagad <br />
-          <input type="radio" name="MobileBank" value="Rocket"> Rocket <br />
-          <input type="radio" name="MobileBank" value="Upai"> Upai <br />
-          <input type="radio" name="MobileBank" value="Fast Pay"> Fast Pay <br />
-          <input type="submit" name="submit" value="Select" >
+          <label>Username:</label><br />
+          <input type="text" name="username" placeholder="Enter your name"><br />
+          <label>Password:</label><br />
+          <input type="password" name="password" placeholder="Enter your password"><br />
+          <label>Age:</label><br />
+          <input type="number" name="age" placeholder="Enter your age"><br /><br />
+          <input type="submit" name="submit" value="Login">
      </form>
 
 
@@ -26,29 +27,16 @@
 <?php
 
 if(isset($_POST['submit'])){
-     $MobileBank = null;
-
-     if(isset($_POST['MobileBank'])){
-          $MobileBank = $_POST['MobileBank'];
-     }
-
-     if($MobileBank == "Bkash"){
-          echo "You have selected: " . $MobileBank . "<br>";
-     }
-     if($MobileBank == "Nagad"){
-          echo "You have selected: " . $MobileBank . "<br>";
-     }
-     if($MobileBank == "Rocket"){
-          echo "You have selected: " . $MobileBank . "<br>";
-     }
-     if($MobileBank == "Upai"){
-          echo "You have selected: " . $MobileBank . "<br>";
-     }
-     if($MobileBank == "Fast Pay"){
-          echo "You have selected: " . $MobileBank . "<br>";
+     if( !empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['age']) ){
+          $username = $_POST['username'];
+          $password = $_POST['password'];
+          $age = $_POST['age'];
+          echo "You are {$username} and you {$age} years old.<br>  Your pass: {$password}";
+          
+     }else{
+          echo "__Please fill up all field";
      }
 }
-
 
 
 ?>
