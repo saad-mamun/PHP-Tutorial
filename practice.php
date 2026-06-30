16981f8e3b7cff09e5e72dc2cf36bf756cb89088
saad-mamun <mamunali2660@gmail.com>
2026-06-30 07:56:04 +0600

    done

--- practice.php
+++ practice.php
@@ -24,36 +24,30 @@
 </html>
 
 <?php
-if (isset($_POST['submit'])) {
 
-
-     if (isset($_POST['pizza'])) {
-          echo "You like pizza!<br>";
+if(isset($_POST['submit'])){
+     if(isset($_POST['pizza'])){
+          echo "You like pizza!  <br>" ;
+     }elseif(isset($_POST['hamburger'])){
+          echo "You like hamburger  <br>";
+     }elseif(isset($_POST['hotdog'])){
+          echo "You like hotdog  <br>";
+     }elseif(isset($_POST['taco'])){
+          echo "You like taco <br> " ;
+     };
+     if(empty($_POST['pizza'])){
+          echo "You don't like pizza! <br>";
      }
-     if (isset($_POST['hamburger'])) {
-          echo "You like hamburger!<br>";
+     if(empty($_POST['hamburger'])){
+          echo "You don't like hamburger <br>" ;
      }
-     if (isset($_POST['hotdog'])) {
-          echo "You like hotdog!<br>";
+     if(empty($_POST['hotdog'])){
+          echo "You don't like hotdog <br>" ; 
      }
-     if (isset($_POST['taco'])) {
-          echo "You like taco!<br>";
-     }
-
-     if (empty($_POST['pizza'])) {
-          echo "You don't like pizza!<br>";
-     }
-     if (empty($_POST['hamburger'])) {
-          echo "You don't like hamburger!<br>";
-     }
-     if (empty($_POST['hotdog'])) {
-          echo "You don't like hotdog!<br>";
-     }
-     if (empty($_POST['taco'])) {
-          echo "You don't like taco!<br>";
+     if(empty($_POST['taco'])){
+          echo "You don't like taco <br>" ;
      }
 }
-
 
 
 ?>
