7733d3c2332d80bf264267587a9cd34f7a88855e
saad-mamun <mamunali2660@gmail.com>
2026-07-01 07:57:12 +0600

    paramiter

--- practice.php
+++ practice.php
@@ -1,34 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Log In</title>
-</head>
-
-<body>
-
-     <form action="practice.php" method="post">
-          <input type="checkbox" name="foods[]" value="Pizza"> Pizza <br>
-          <input type="checkbox" name="foods[]" value="Hamburger"> Hamburger <br>
-          <input type="checkbox" name="foods[]" value="HotDog"> HotDog <br>
-          <input type="checkbox" name="foods[]" value="Taco"> Taco <br>
-          <input type="submit" name="submit" value="Submit">
-     </form>
-
-</body>
-</html>
-
 <?php
-
-if(isset($_POST['submit'])){
-  $foods = $_POST['foods'];
-  foreach($foods as $food){
-     echo "You liked $food  <br>";
-  }
-
+function name($first_name, $last_name){
+     echo "My name is $first_name $last_name <br>";
 }
+name("Liton", "Al-Mamun");
+
 
 
 ?>
