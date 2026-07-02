0beeda3536c09a881931b822f74f26c3b02d5f55
saad-mamun <mamunali2660@gmail.com>
2026-07-02 08:27:33 +0600

    sanitize prac

--- practice.php
+++ practice.php
@@ -1,19 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="practice.php" method="post" >
+        <input type="text" name="username" placeholder="Enter your name" ><br>
+        <input type="email" name="email" placeholder="Enter your email"><br>
+        <input type="number" name="age" placeholder="Enter your age"><br> 
+        <input type="submit" name="login" value="Login"><br>
+    </form>
+</body>
+</html>
 <?php
-$name = "liton al mamun";
 
-// $name = trim($name);
-// $name = strtoupper($name);
-// $name = strtolower($name);
-// $name = strlen($name);
-// $name =ucwords($name);
-// $name= ucfirst($name);
-$name =strpos($name, "al");
-// $name = substr($name,5,15);
-// $name = strrev($name);
-// $name = str_replace($name,"" , "Naima");
-// $name = str_repeat($name, 5);
-// $name = str_shuffle($name);
-// $name = str_pad($name, 20, "*");
-// $name = str_word_count($name);
+if(isset($_POST['login'])){
+    $username =$_POST['username'];
+    $email = $_POST['email'];
+    $age = $_POST['age'];
 
-echo "My name is: " . $name;
+    echo "Hello {$username} <br> Email: {$email} <br>  Age: {$age} ";
+}
