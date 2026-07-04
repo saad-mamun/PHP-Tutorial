8a1f4c4559c7e61bb32c51a5548e79090773b67f
saad-mamun <mamunali2660@gmail.com>
2026-07-04 22:13:56 +0600

    complite

--- practice.php
+++ practice.php
@@ -8,21 +8,9 @@
 <body>
     <form action="practice.php" method="post" >
         <input type="text" name="name" placeholder="Enter your name" ><br>
-        <input type="email" name="email" placeholder="Enter your email"><br>
-   
-        <input type="submit" name="login" value="Login"><br>
+        <input type="email" name="email" placeholder="Enter your email">
     </form>
 </body>
 </html>
 <?php
 
-if(isset($_POST['login'])){
- 
-    $username = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
-    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
-    $age = filter_input(INPUT_POST, "age", FILTER_SANITIZE_NUMBER_INT);
-    echo "Hello {$username} <br>";
-    echo "Email: {$email} <br>";
-
-
-}
