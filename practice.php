bc790bb52c28c19e753e1edf4555535c4ae7895c
saad-mamun <mamunali2660@gmail.com>
2026-07-06 22:40:30 +0600

    A

--- practice.php
+++ practice.php
@@ -1,29 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="practice.php" method="post">
-        <input type="text" name="username" placeholder="Enter your username"><br>
-        <input type="email" name="email" placeholder="Enter your email"><br>
-        <input type="number" name="age" placeholder="Enter your age"><br>
-        <input type="submit" name="submit" value="Submit">
-    </form>
-</body>
-</html>
 <?php
+$cookie_name = "user";
+$cookie_value = "Alex Porter";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+?>
 
+<html>
 
-if(isset($_POST['submit'])){
+<body>
+    <?php
+    if (isset($_COOKIE[$cookie_value])) {
+        echo "Cookie '" . $cookie_name . "' is set!<br>";
+        echo "Value is: " . $_COOKIE[$cookie_name];
+    } else {
+        echo "Cookie named '" . $cookie_value . "' is not set!";
+    }
+    ?>
+</body>
 
-$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-$age = filter_input(INPUT_POST, "age", FILTER_VALIDATE_INT);
-
-if(empty($age)){
-    echo "Please enter a valid age.";
-}
-
-};
+</html>
