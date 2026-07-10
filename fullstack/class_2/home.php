88bf879b7ea73511ee55e3074c36abd700334e98
saad-mamun <mamunali2660@gmail.com>
2026-07-10 19:23:20 +0600

    scope

---
+++ fullstack/class_2/home.php
@@ -0,0 +1,65 @@
+
+<?php
+//find variable memory location
+
+// class Test {
+//     public $name = "liton";
+// }
+
+// $obj = new Test();
+// echo spl_object_id($obj)
+
+
+
+// PHP has three different variable scopes: local, Global, Super Global.
+
+// { }  ar modhe sob Local Scope
+
+// function greet(){
+
+//     $massage = "Hi! john";
+//     echo $massage;
+// }
+// greet();
+
+
+
+//Global variable
+
+// $name = "Mitu";
+// function grt(){
+//     global $name;
+//     echo "Hello " . $name . "! <br>";
+// }
+// grt();
+// echo "Global Variable " . $name;
+
+//Super Global. Nicher soho aro onk +
+// $_POST, $_GET, $_SERVER,$_SESSION;
+
+
+
+
+
+//Constant
+
+// define("SITE_NAME", "Chaldal");
+// echo SITE_NAME;
+
+// const PI = 3.1416;
+// echo PI;
+
+
+
+
+
+//Class Constant
+// :: ATAKE BOLE SCOPE RESULETIONARI OPARETOR
+
+// class Test{
+//     const PI = 3.1416;
+// }
+// echo Test :: PI;
+
+
+;
--- practice.php
+++ practice.php
@@ -1,15 +1,7 @@
 <?php
 
-$password = "litonALI9118";
 
-$hash = password_hash($password, PASSWORD_DEFAULT);
-
-if(password_verify($password, $hash)){
-    echo "Password is Verify";
-}else{
-    echo "Invalid Password";
-}
-
+//variable location
 
 
 ?>
