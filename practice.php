3705eeec6a936ed971068d227fd30f09fbbdb495
saad-mamun <mamunali2660@gmail.com>
2026-07-10 18:22:59 +0600

    password hash

--- practice.php
+++ practice.php
@@ -1,3 +1,15 @@
 <?php
-echo "Practice make perfect"
+
+$password = "litonALI9118";
+
+$hash = password_hash($password, PASSWORD_DEFAULT);
+
+if(password_verify($password, $hash)){
+    echo "Password is Verify";
+}else{
+    echo "Invalid Password";
+}
+
+
+
 ?>
