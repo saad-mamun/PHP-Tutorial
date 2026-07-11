809dd246fca69f9caac984ba210d8588be2418bd
saad-mamun <mamunali2660@gmail.com>
2026-07-11 19:19:04 +0600

    ternary operator

--- fullstack/class 3/home.php
+++ fullstack/class 3/home.php
@@ -18,15 +18,35 @@ echo "<br>";
 
 // Nested if else
 
-$marksNested = 64;
+$marksNested = 67;
 
-if($marksNested < 33 ){
+if($marksNested<33){
     echo "You fail";
 }else{
-    if($marksNested < 60 ){
+    if($marksNested<60 ){
         echo "Average";
+    }else{
+        if($marksNested<50){
+            echo "D";
+        }
     }
 }
 
 
-?>
+
+echo "<br>";
+
+//ternary operator
+
+$age = 550;
+
+if ($age > 18){
+    echo "ADULT";
+}else{
+    echo "Minor";
+}
+
+echo "<br>";
+
+$minorityCheck = ($age >= 18)? "You are adult" : "You are child";
+echo "{$minorityCheck}";
