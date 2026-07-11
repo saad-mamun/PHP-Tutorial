c5bd0299d94d1b91ccf4094a81c4041a568e92a3
saad-mamun <mamunali2660@gmail.com>
2026-07-11 19:06:32 +0600

    nested if

--- fullstack/class 3/home.php
+++ fullstack/class 3/home.php
@@ -13,4 +13,20 @@ if ($marks >= 80){
     echo "Unknown grade";
 }
 
+
+echo "<br>";
+
+// Nested if else
+
+$marksNested = 64;
+
+if($marksNested < 33 ){
+    echo "You fail";
+}else{
+    if($marksNested < 60 ){
+        echo "Average";
+    }
+}
+
+
 ?>
