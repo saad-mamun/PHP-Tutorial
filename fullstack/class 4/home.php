6f1df855883286df03b86a63234713a29320df50
saad-mamun <mamunali2660@gmail.com>
2026-07-13 18:19:27 +0600

    Basic authentication system

--- fullstack/class 4/home.php
+++ fullstack/class 4/home.php
@@ -2,13 +2,36 @@
 <?php
 //Here you can see projects
 
-echo "Enter a number: ";
-$number = (int)readline(); // readline holo terminal theke user er input receive kore.
+// Project: 1 - 
 
-if ($number > 0) {
-    echo "The number is positive.";
-} elseif ($number < 0) {
-    echo "The number is Negative";
-} else {
-    echo " The number is zero";
+
+// echo "Enter a number: ";
+// $number = (int)readline(); // readline holo terminal theke user er input receive kore.
+
+// if ($number > 0) {
+//     echo "The number is positive.";
+// } elseif ($number < 0) {
+//     echo "The number is Negative";
+// } else {
+//     echo " The number is zero";
+// }
+
+// PROJECT: 2 - Basic authentication system..
+
+define("USERNAME", "admin");
+define("PASSWORD", "2315DF");
+
+
+echo "Enter username: ";
+$inputUsername = readline();
+
+echo "Enter password: ";
+$inputPassword = readline();
+
+
+if( $inputUsername === USERNAME && $inputPassword === PASSWORD  ){
+    echo "Login Successful";
+}else{
+    echo "Invalid username or password";
 }
+
