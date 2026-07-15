60e36ca47169069cc53422c38982b3f6f64bd7c4
saad-mamun <mamunali2660@gmail.com>
2026-07-15 19:04:06 +0600

    local global cope and other func

--- fullstack/class 6/home.php
+++ fullstack/class 6/home.php
@@ -41,7 +41,7 @@
 //     return $sum;  
 // } 
 // echo add(1, 2, 3, 4);  
- 
+
 
 // INTIGER NUMBER
 // function sumOfTwoNumbersInt(int $num1, int $num2) : int{
@@ -62,8 +62,51 @@
 
 //float othoba int
 // Union return type
-function sumOfTwoNumbersInt( $num1,  $num2) : float | int{
-    $result = $num1 + $num2;
-    return $result;
-}
-echo sumOfTwoNumbersInt(30,5.60);
+// function sumOfTwoNumbersInt( $num1,  $num2) : float | int{
+//     $result = $num1 + $num2;
+//     return $result;
+// }
+// echo sumOfTwoNumbersInt(30,5.60);
+
+
+//local scope variable
+// function variableScopeLocal(){
+//     $nameLocal = "liton";
+//     echo "Hello, I am $nameLocal  \n";
+// }
+// variableScopeLocal();
+
+
+// //global scope variable
+
+// $staff = "Tanmoy";
+// function variableScopeGlobal(){
+//     global $staff;
+//     echo "$staff please make a cup of coffee \n" ;
+// }
+// variableScopeGlobal();
+
+
+
+// function typeHintPri( int $a, float $b, string $c ){
+//     $d = $a + $b;
+//     return "$c result is $d \n";
+// }
+// echo typeHintPri(45,52.05,"Naima's");
+
+
+
+// function typeHintArray(array $numbers){
+//     return array_sum($numbers);
+// }
+
+// echo typeHintArray([12,52,9,78.2]);
+
+
+// function typeHintNull(? string $name) : string{
+//     return $name ?? "Unknown Gust \n";
+// }
+// echo typeHintNull(null);
+
+
+//Anonymous Function
