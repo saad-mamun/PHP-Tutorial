646b3443a0d29f439e2b53e78f24f1d6b1ac1c74
saad-mamun <mamunali2660@gmail.com>
2026-07-15 18:22:26 +0600

    / Union return type

--- fullstack/class 6/home.php
+++ fullstack/class 6/home.php
@@ -26,19 +26,44 @@
 
 //Variadic Function
 
-function sumOfNumber(...$nums){
-    $sumResult = array_sum($nums);
-    return $sumResult;
+// function sumOfNumber(...$nums){
+//     $sumResult = array_sum($nums);
+//     return $sumResult;
+// }
+// echo sumOfNumber(10,30,54);
+
+
+// function add(...$nums) {  
+//     $sum = 0;  
+//     foreach ($nums as $n) {  
+//         $sum += $n;  
+//     }  
+//     return $sum;  
+// } 
+// echo add(1, 2, 3, 4);  
+ 
+
+// INTIGER NUMBER
+// function sumOfTwoNumbersInt(int $num1, int $num2) : int{
+//     $result = $num1 + $num2;
+//     return $result;
+// }
+// echo sumOfTwoNumbersInt(30,50); 
+
+
+//FLOAT NUMBER
+// function sumOfTwoNumbersInt(float $num1, float $num2) : float{
+//     $result = $num1 + $num2;
+//     return $result;
+// }
+// echo sumOfTwoNumbersInt(30.52,50);
+
+
+
+//float othoba int
+// Union return type
+function sumOfTwoNumbersInt( $num1,  $num2) : float | int{
+    $result = $num1 + $num2;
+    return $result;
 }
-echo sumOfNumber(10,30,54);
-
-
-function add(...$nums) {  
-    $sum = 0;  
-    foreach ($nums as $n) {  
-        $sum += $n;  
-    }  
-    return $sum;  
-} 
-echo add(1, 2, 3, 4);  
-?> 
+echo sumOfTwoNumbersInt(30,5.60);
