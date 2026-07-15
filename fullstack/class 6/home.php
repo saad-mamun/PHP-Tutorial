ae2c9de6d31605189308109a43b6cdcce39e449c
saad-mamun <mamunali2660@gmail.com>
2026-07-15 17:33:07 +0600

    Variadic Function

--- fullstack/class 6/home.php
+++ fullstack/class 6/home.php
@@ -6,12 +6,39 @@
 
 
 
-function someOfTwoNumber($num1, $num2){
-    $result = $num1 + $num2;
-    return $result;
-}
-$sumTest1 =  someOfTwoNumber(20,50);
-$sumTest2 =  someOfTwoNumber(10,20);
+// function someOfTwoNumber($num1, $num2){
+//     $result = $num1 + $num2;
+//     return $result;
+// }
+// $sumTest1 =  someOfTwoNumber(20,50);
+// $sumTest2 =  someOfTwoNumber(10,20);
 
-$myResult = $sumTest1 - $sumTest2;
-echo $myResult;
+// $myResult = $sumTest1 - $sumTest2;
+// echo $myResult;
+
+
+// function greeting($name = "Sir"){
+//     echo "Wellcome $name";
+// }
+//  greeting(); // oi defalt er name = sir show korebe
+//  greeting("Naima");
+
+
+//Variadic Function
+
+function sumOfNumber(...$nums){
+    $sumResult = array_sum($nums);
+    return $sumResult;
+}
+echo sumOfNumber(10,30,54);
+
+
+function add(...$nums) {  
+    $sum = 0;  
+    foreach ($nums as $n) {  
+        $sum += $n;  
+    }  
+    return $sum;  
+} 
+echo add(1, 2, 3, 4);  
+?> 
