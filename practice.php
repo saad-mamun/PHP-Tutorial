ff072f2be68afca30780b75d5e46d6ef768dbd17
saad-mamun <mamunali2660@gmail.com>
2026-07-18 23:41:07 +0600

    default funtion

--- practice.php
+++ practice.php
@@ -1,12 +1,8 @@
 <?php
 
 
-function sum($num1, $num2){
-    $result = $num1 ** $num2;
-    return $result;
+function deff($name = "Sir"){
+    echo "Welcome $name ";
 }
-
-$sumTest1 = sum(3,3);
-$sumTest2 = sum(2,3);
-$finalResult = $sumTest1 - $sumTest2;
-echo $finalResult;
+// deff();
+deff("Naima");
