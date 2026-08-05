039d7340a81ae7408c585cab0116ded8ee44f1e8
saad-mamun <mamunali2660@gmail.com>
2026-08-05 05:08:26 -0700

    for loop

--- practice.php
+++ practice.php
@@ -1,14 +1,3 @@
-<?php
-
-$marks = 50;
-
-if($marks <= 100 && $marks >= 80){
-    echo "A+";
-}elseif($marks <=50){
-    echo "B";
-
-
-};
-
-
-$adultCheck = ($age >= 18) ? "You are adult" : "You are still a child";
+for($i = 1; $i <= 10; $i++ ){
+    echo $i . "\n";
+}
