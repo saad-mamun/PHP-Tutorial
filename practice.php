19b79ab6dcb7a1c3f0530d6a35dc71e9e4e62cd8
saad-mamun <mamunali2660@gmail.com>
2026-08-07 05:39:33 -0700

    while loop

--- practice.php
+++ practice.php
@@ -1,16 +1,13 @@
 <?php
 
-echo "Enter you unites: ";
-$unites = (int)readline();
-
-
-if($unites <= 100){
-    $bill = $unites * 5;
-} elseif($unites <= 200){
-    $bill = 100 * 5 + ($unites - 100) * 10;
-}elseif($unites > 200){
-    $bill = (100 * 5) + (100* 10) + ($unites - 200) * 15;
-}else{
-    echo "You can not consumed any unite";
+$i = 1;
+while($i <= 5){
+    echo $i . "\n";
+    $i++;
 }
-echo "Your bill is: $" .$bill;
+
+
+
+
+
+?>
