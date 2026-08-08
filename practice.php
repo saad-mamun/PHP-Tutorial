01e10bb4863771bb2076cb905833e75c4c274bb7
saad-mamun <mamunali2660@gmail.com>
2026-08-07 19:32:56 -0700

    foreach all method

--- practice.php
+++ practice.php
@@ -1,11 +1,21 @@
 <?php
 
-$i = 5;
-
-do{
-    echo $i . "\n";
-    $i++;
-}while($i <= 5);
+$colors = ["red", "green", "blue", "yellow", "pink", "purple", "black", "lime", "orange"];
 
 
-?>
+for($i = 0; $i < count($colors); $i++){
+    // echo "Colors name is: " . $colors[$i]. "\n";
+}
+
+foreach($colors as $color){
+    // echo $color . "\n";
+};
+
+foreach($colors as $index => $color){
+//   echo $index . " - " . $color ."\n" ;  
+};
+
+
+foreach($colors as $index => $color){
+    echo $index + 1 . " - ". $color . "\n";
+}
