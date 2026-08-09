c0a424ac709440292965115f7a00ce8d514e8ffe
saad-mamun <mamunali2660@gmail.com>
2026-08-08 18:41:48 -0700

    float and int

--- practice.php
+++ practice.php
@@ -1,8 +1,16 @@
 <?php
 
-function sum(...$nums){
-    $result = array_sum($nums);
+
+function sumOfInt (int $num1, int $num2) : int{
+    $result = $num1 + $num2;
+    return $result;
+}
+echo sumOfInt(10,20.5);
+echo "</br>";
+
+function sumOfFloat(float $num1, float $num2) : float{
+    $result = $num1 + $num2;
     return $result;
 }
 
-echo sum(10,20,30,40);
+echo sumOfFloat(12.2,2.3);
