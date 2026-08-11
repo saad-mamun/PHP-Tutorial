83d29fe4d548c4d88b66ad6179b8cc6adb4306ea
saad-mamun <mamunali2660@gmail.com>
2026-08-10 20:06:00 -0700

    solve view section

--- practice.php
+++ practice.php
@@ -2,14 +2,14 @@
 
 $contacts = [];
 
-function addContact(array $contacts, string $name, string $email, string $phone): void
+function addContact(array &$contacts, string $name, string $email, string $phone): void
 {
     $contacts[] = ['name' => $name, 'email' => $email, 'phone' => $phone];
 }
 
 
 //display function
-function displayFunction(array $contacts): void
+function displayContact(array $contacts): void
 {
     if (empty($contacts)) {
         echo "No contact available. \n";
@@ -32,10 +32,12 @@ while (true) {
         $email = readline("Enter email: ");
         $phone = readline("Enter phone: ");
         addContact($contacts, $name, $email, $phone);
+        echo "$name added successfully\n";
     } elseif ($choice === 2) {
-        displayFunction($contacts);
+        displayContact( $contacts);
     } elseif ($choice == 3) {
         echo "Exiting...\n";
+        break;
     } else {
         echo "Invalid choice. Please try again.\n";
     }
