8778dd485b1b6ae3e42c84a92084b071793e2f8f
saad-mamun <mamunali2660@gmail.com>
2026-08-12 03:23:31 -0700

    associative array

--- fullstack/class 8/index.php
+++ fullstack/class 8/index.php
@@ -1,7 +1,27 @@
 <?php
 
+
+// index array....
 $array1 = [1,2,3,4,5,6];
 
 for($i = 0; $i < count($array1); $i++){
     echo $array1[$i] . " \n";
+}
+
+$array1 = [1,2,3,4,5,6];
+
+foreach($array1 as $value){
+    echo $value . "\n";
+}
+
+
+//Associative array....
+$person1 = array(
+    "first_name" => "Naima",
+    "last_name" => "Islam",
+    "age" => "23"
+);
+
+foreach($person1 as $key => $item){
+    echo "$key : $item  \n" ;
 }
--- practice.php
+++ practice.php
@@ -1,7 +1,2 @@
 <?php
 
-$array1 = [1,2,3,4,5,6];
-
-for($i = 0; $i < count($array1); $i++){
-    echo $array1[$i] . " \n";
-}
