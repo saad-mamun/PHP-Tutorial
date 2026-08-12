2494eb41603e735e74695e1c1e77d648f4c44408
saad-mamun <mamunali2660@gmail.com>
2026-08-12 10:35:40 -0700

    multiDimensional array

--- fullstack/class 8/index.php
+++ fullstack/class 8/index.php
@@ -25,3 +25,20 @@ $person1 = array(
 foreach($person1 as $key => $item){
     echo "$key : $item  \n" ;
 }
+
+
+
+// Multidimensional array....
+$array1 = array(
+   array(1,2,3), 
+   array(4,5,6), 
+   array(7,8,9), 
+);
+
+print_r($array1[0]);
+print_r($array1[1]);
+print_r($array1[2]);
+
+// array1 er mothde thaka array gulor value
+echo $array1[0][1] . "\n" ;
+echo $array1[1][2];
--- practice.php
+++ practice.php
@@ -1,2 +1,17 @@
 <?php
 
+$array1 = array(
+   array(1,2,3), 
+   array(4,5,6), 
+   array(7,8,9), 
+);
+
+// print_r($array1[0]);
+// print_r($array1[1]);
+// print_r($array1[2]);
+
+
+// array1 er mothde thaka array gulor value
+// echo $array1[0][1];
+echo $array1[1][2];
+
