fb6d0662a8c66770d3dbf6975cfcbb6bbec5d946
saad-mamun <mamunali2660@gmail.com>
2026-08-15 18:55:03 -0700

    php methods

---
+++ fullstack/class_9/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$name = "Hello, I am Liton";
+
+$length = strlen($name);
+
+// echo $length;
+
+// echo str_word_count($name);
+
+// echo strrev($name);
+
+// echo strpos($name, "am");
+
+// echo substr($name, 0,5);
+
+// echo str_replace( "Liton", "Naima", $name);
+
+echo strtolower($name);
