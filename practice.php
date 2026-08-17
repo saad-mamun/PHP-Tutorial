5d36f246d547864330969a756225c32f59c2b268
saad-mamun <mamunali2660@gmail.com>
2026-08-16 19:00:43 -0700

    slice

--- practice.php
+++ practice.php
@@ -2,5 +2,8 @@
 
 $fruits = ["apple", "banana", "orange", "grape", "kiwi"];
 
-array_splice($fruits, 1,2);
-print_r($fruits);
+// array_splice($fruits, 1,2, array("fruit1", "fruit2"));
+// print_r($fruits);
+
+$oparetion = array_slice($fruits, 1, 3);
+print_r($oparetion);
