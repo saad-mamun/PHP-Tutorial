2900843f01c734ab0ddc1bc46c362aa672a038d0
saad-mamun <mamunali2660@gmail.com>
2026-08-22 03:43:55 -0700

    file content

--- fullstack/class_9/index.php
+++ fullstack/class_9/index.php
@@ -29,9 +29,31 @@ $length = strlen($name);
 // echo trim($fullName);
 
 //important - hash kore md5 dia
-$password = "MilonHardware@32315}";
-echo md5($password);
+// $password = "MilonHardware@32315}";
+// echo md5($password);
 
 
 // FILE-
 
+// $file = "example.text";
+// $content = "This is text content for example file";
+
+// if(file_put_contents($file, $content)){
+//     echo "File created successfully";
+// }else{
+//     echo "Failed to create File";
+// }
+
+// // Athoba...
+
+// $fileTwo = "second-file.txt";
+// // w-means write
+// $handle = fopen($fileTwo, 'w');
+
+// if($handle){
+//     fwrite($handle, "Hello I am second file");
+//     fclose($handle);
+//     echo "Second file created successfully";
+// }else{
+//     echo "Failed" ;
+// }
