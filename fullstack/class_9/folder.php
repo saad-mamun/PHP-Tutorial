99bde49192aaed7a17242e32b5a78506b9dde9d4
saad-mamun <mamunali2660@gmail.com>
2026-08-24 18:52:47 -0700

    read delete

--- fullstack/class_9/folder.php
+++ fullstack/class_9/folder.php
@@ -2,12 +2,48 @@
 
 $folderName = "Students";
 
-if(!file_exists($folderName)){
-    if(mkdir($folderName, 0777, true)){
-        echo "folder create successfully";
-    }else {
-        echo "Failed to create folder" ;
+
+//create
+
+// if(!file_exists($folderName)){
+//     if(mkdir($folderName, 0777, true)){
+//         echo "folder create successfully";
+//     }else {
+//         echo "Failed to create folder" ;
+//     }
+// }else{
+//     echo "folder already exists";
+// };
+
+
+
+
+//read
+
+// if(is_dir($folderName)){
+//     $files = scandir($folderName);
+//     print_r($files);
+// }else{
+//     echo "Folder dose not exists";
+// }
+
+
+//delete
+// 1st file create kora
+
+// if (mkdir('Test_dir', 0777, true)) {
+//     echo "folder create successfully";
+// } else {
+//     echo "Failed to create folder";
+// }
+
+$deleteEmptyDir = 'Test_dir';
+if(is_dir($deleteEmptyDir)){
+    if(rmdir($deleteEmptyDir)){
+        echo "Folder successfully deleted";
+    }else{
+        echo "Failed";
     }
 }else{
-    echo "folder already exists";
-};
+    echo "Not found";
+}
