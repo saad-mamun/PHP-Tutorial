e190c942d14aa6ef2302c9f116374b8a2f47022d
saad-mamun <mamunali2660@gmail.com>
2026-08-27 20:56:17 -0700

    session destroy()

--- fullstack/class_9/session.php
+++ fullstack/class_9/session.php
@@ -3,5 +3,15 @@
 session_start();
 
 $_SESSION['username'] = 'Sobuj';
+$_SESSION['userId'] = '2157419';
 
+echo $_SESSION['username'];
+echo "<br>";
+echo $_SESSION['userId'];
+
+
+session_unset();
+session_destroy();
+echo "<br>";
+echo "After Destroy";
 echo $_SESSION['username'];
