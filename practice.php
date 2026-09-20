ee1763efcd8bf819cf761e223ba2f4d89cb52644
saad-mamun <mamunali2660@gmail.com>
2026-09-19 19:13:05 -0700

    define

--- practice.php
+++ practice.php
@@ -1,17 +1,5 @@
 <?php
 
 
-echo "Enter unites you consumed: ";
-$units = (int)readline();
-
-
-if ($units <= 100) {
-    $bill = $units * 5;
-} elseif ($units <= 200) {
-    $bill = 100 * 5 + ($units - 100) * 10;
-} elseif ($units > 200) {
-    $bill = (100 * 5) + (100 * 10) + ($units - 200) * 15;
-} else {
-    echo "You can not consumed any unite";
-}
-echo "Your total Bill: $bill";
+define("FACTOR", 9 / 5);
+define("OFFSET", 32);
