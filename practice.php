bcde5fca364f6ca7e62f98b0f99e99e9a5d1bf65
saad-mamun <mamunali2660@gmail.com>
2026-09-20 18:50:50 -0700

    define username and password

--- practice.php
+++ practice.php
@@ -1,23 +1,5 @@
 <?php
 
 
-define("FACTOR", 9 / 5);
-define("OFFSET", 32);
-
-echo "Enter a temperature value: ";
-$temperature = (float)readline();
-
-echo "Convert to (1: Fahrenheit, 2: Celsius ) : ";
-$choice = (int)readline();
-switch ($choice) {
-    case 1:
-        $result = $temperature * FACTOR + OFFSET;
-        echo "Temperature in Fahrenheit: $result";
-        break;
-    case 2:
-        $result = ($temperature - OFFSET) / FACTOR;
-        echo "Temperature in Celsius: $result";
-        break;
-    default:
-        echo "Invalid choice";
-}
+define("USERNAME", "admin");
+define("PASSWORD", "2315DF");
