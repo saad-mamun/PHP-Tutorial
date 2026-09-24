d559e7abe094937461e7f135ddd3897294a74bcc
saad-mamun <mamunali2660@gmail.com>
2026-09-23 19:11:31 -0700

    echo temp

--- practice.php
+++ practice.php
@@ -3,3 +3,9 @@
 
 define("FACTOR", 9 / 5);
 define("OFFSET", 32);
+
+echo "Enter a temperature value: ";
+$temperature = (float)readline();
+
+echo "Convert to (1: Fahrenheit, 2: Celsius ) : ";
+$choice = (int)readline();
