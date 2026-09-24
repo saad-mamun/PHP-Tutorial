743eeae5d55e258dabe96f5a89c126e6bb6ecdf7
saad-mamun <mamunali2660@gmail.com>
2026-09-23 19:11:02 -0700

    define factor

--- practice.php
+++ practice.php
@@ -1,17 +1,5 @@
 <?php
 
 
-define("USERNAME", "admin");
-define("PASSWORD", "2315DF");
-
-echo "Enter username: ";
-$inputUsername = readline();
-
-echo "Enter password: ";
-$inputPassword = readline();
-
-if( $inputUsername === USERNAME && $inputPassword === PASSWORD  ){
-    echo "Login Successful";
-}else{
-    echo "Invalid username or password";
-}
+define("FACTOR", 9 / 5);
+define("OFFSET", 32);
