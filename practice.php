9b829cdfbf785e833ad4399465f8abfb6344bc9e
saad-mamun <mamunali2660@gmail.com>
2026-09-24 19:22:41 -0700

    all colors

--- practice.php
+++ practice.php
@@ -4,4 +4,7 @@
 $colors = ["red", "green", "blue", "yellow", "pink", "purple", "black", "lime", "ash"];
 
 
-echo count($colors);
+// echo count($colors);
+for($i = 0; $i < count($colors); $i++){
+    echo $colors[$i] . "\n";
+};
