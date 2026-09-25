06f298739cf23af291da1f5e090e647f317e87a9
saad-mamun <mamunali2660@gmail.com>
2026-09-24 19:20:59 -0700

    count

--- practice.php
+++ practice.php
@@ -1,25 +1,7 @@
 <?php
 
 
-define("FACTOR", 9 / 5);
-define("OFFSET", 32);
-
-echo "Enter a temperature value: ";
-$temperature = (float)readline();
-
-echo "Convert to (1: Fahrenheit, 2: Celsius ) : ";
-$choice = (int)readline();
+$colors = ["red", "green", "blue", "yellow", "pink", "purple", "black", "lime", "ash"];
 
 
-switch ($choice) {
-    case 1:
-        $result = $temperature * FACTOR + OFFSET;
-        echo "Temperature in Fahrenheit: $result";
-        break;
-    case 2:
-        $result = ($temperature - OFFSET) / FACTOR;
-        echo "Temperature in Celsius: $result";
-        break;
-    default:
-        echo "Invalid choice";
-}
+echo count($colors);
